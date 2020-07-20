767f77d68c83037455ab15dc8f8f9dcd593603ab
edzdez <ezsland05@gmail.com>
2020-07-20 15:56:19 -0700

    Added /spawn functionality

--- src/main/java/com/edzdez/vanillaplus/VanillaPlus.java
+++ src/main/java/com/edzdez/vanillaplus/VanillaPlus.java
@@ -3,6 +3,7 @@ package com.edzdez.vanillaplus;
 import com.edzdez.vanillaplus.commands.craftCommand;
 import com.edzdez.vanillaplus.commands.flyCommand;
 import com.edzdez.vanillaplus.commands.healCommand;
+import com.edzdez.vanillaplus.commands.spawnCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class VanillaPlus extends JavaPlugin {
@@ -14,6 +15,7 @@ public final class VanillaPlus extends JavaPlugin {
         new healCommand(this);
         new flyCommand(this);
         new craftCommand(this);
+        new spawnCommand(this);
     }
 
     @Override
--- src/main/java/com/edzdez/vanillaplus/commands/spawnCommand.java
+++ src/main/java/com/edzdez/vanillaplus/commands/spawnCommand.java
@@ -1,37 +1,40 @@
 package com.edzdez.vanillaplus.commands;
 
 import com.edzdez.vanillaplus.VanillaPlus;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class flyCommand implements CommandExecutor {
+public class spawnCommand implements CommandExecutor {
     private VanillaPlus plugin;
 
-    public flyCommand(VanillaPlus plugin) {
+    public spawnCommand(VanillaPlus plugin) {
         this.plugin = plugin;
-        plugin.getCommand("fly").setExecutor(this);
+        plugin.getCommand("spawn").setExecutor(this);
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (!(sender instanceof Player)) {
             sender.sendMessage("Sorry, Console can't do this!");
             return true;
         }
-
         Player p = (Player) sender;
 
-        if (p.hasPermission("vanillaplus.fly")) {
-            p.setAllowFlight(true);
-            if (p.isFlying()) p.setAllowFlight(false);
-            else p.setAllowFlight(true);
-            p.sendMessage("Toggling Flight...");
-            return true;
+        if (p.hasPermission("vanillaplus.spawn")) {
+            try {
+                p.teleport(p.getBedSpawnLocation());
+            } catch (Exception e) {
+                Location sp = new Location(p.getWorld(), p.getWorld().getSpawnLocation().getX(), p.getWorld().getHighestBlockYAt(p.getWorld().getSpawnLocation()) + 2, p.getWorld().getSpawnLocation().getZ());
+
+                p.teleport(sp);
+            }
         } else {
             p.sendMessage("Sorry, you do not have permission to use this command. If you think this is a mistake, please contact your server operators.");
         }
+
         return false;
     }
 }
